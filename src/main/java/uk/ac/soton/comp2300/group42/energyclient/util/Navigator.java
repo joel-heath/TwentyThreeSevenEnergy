bf79c482fe0acf54777e699572fb72b45ae0bb6b
Joel Heath <joelheath@outlook.com>
2026-01-30 18:03:45 +0000

    Updated Navigator to allow callback functions for setting up Controllers. Added default null overloads as most instances don't require it. This allows dynamic pages relating to specific entities, e.g. an edit reminder page or manage specific housemate page. Note that it should only be used for specific entities, not a group of entities, i.e. when running any findAll operation, it should be done through a repository on the ViewModel.

--- src/main/java/uk/ac/soton/comp2300/group42/energyclient/util/Navigator.java
+++ src/main/java/uk/ac/soton/comp2300/group42/energyclient/util/Navigator.java
@@ -12,18 +12,32 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.Objects;
 import java.util.Stack;
+import java.util.function.Consumer;
 
 public class Navigator {
+    private static class ViewContext {
+        final String fxmlPath;
+        final Consumer<Object> controllerSetup;
+
+        // We use unchecked casting here because we trust that the Consumer passed
+        // matches the Controller of the FXML path.
+        @SuppressWarnings("unchecked")
+        ViewContext(String path, Consumer<?> setup) {
+            this.fxmlPath = path;
+            this.controllerSetup = (Consumer<Object>) setup;
+        }
+    }
+
     private static Scene mainScene;
-    private static final Stack<String> backHistory = new Stack<>();
-    private static final Stack<String> forwardHistory = new Stack<>();
+    private static final Stack<ViewContext> backHistory = new Stack<>();
+    private static final Stack<ViewContext> forwardHistory = new Stack<>();
     private static final String defaultPath = "/uk/ac/soton/comp2300/group42/energyclient/view/";
     private static final ViewModelFactory vmFactory = new ViewModelFactory();
 
-    private static Parent loadFXML(String fxmlPath) throws IOException {
+    private static <T> Parent loadFXML(ViewContext context) throws IOException {
         URL fxml = Objects.requireNonNull(
-                Navigator.class.getResource(fxmlPath),
-                fxmlPath + " not found"
+                Navigator.class.getResource(context.fxmlPath),
+                context.fxmlPath + " not found"
         );
 
         FXMLLoader loader = new FXMLLoader(fxml);
@@ -35,20 +49,29 @@ public class Navigator {
                         ? controllerClass.getConstructor(viewModel.getClass()).newInstance(viewModel)
                         : controllerClass.getDeclaredConstructor().newInstance();
 
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                     NoSuchMethodException e) {
                 throw new RuntimeException("Failed to instantiate the ViewModel, maybe you need a constructing case in the ViewModelFactory.", e);
             }
         });
 
-        return loader.load();
+        Parent root = loader.load();
+
+        T controller = loader.getController();
+        if (context.controllerSetup != null && controller != null)
+            context.controllerSetup.accept(controller);
+
+        root.setUserData(context);
+
+        return root;
     }
 
-    public static void initializeAbsolute(String fxmlPath, Stage mainStage) throws IOException {
+    public static void initializeAbsolute(String fxmlPath, Stage mainStage) throws IOException { initializeAbsolute(fxmlPath, mainStage, null); }
+    public static <T> void initializeAbsolute(String fxmlPath, Stage mainStage, Consumer<T> controllerSetup) throws IOException {
         if (mainScene != null)
             System.out.println("Warning: You shouldn't call `initialise` more than once");
 
-        Parent root = loadFXML(fxmlPath);
-        root.setUserData(fxmlPath);
+        Parent root = loadFXML(new ViewContext(fxmlPath, controllerSetup));
         mainScene = new Scene(root);
 
         mainScene.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> {
@@ -56,8 +79,7 @@ public class Navigator {
             if (button == MouseButton.BACK) {
                 goBack();
                 event.consume();
-            }
-            else if (button == MouseButton.FORWARD) {
+            } else if (button == MouseButton.FORWARD) {
                 goForward();
                 event.consume();
             }
@@ -66,49 +88,53 @@ public class Navigator {
         mainStage.setScene(mainScene);
     }
 
-    public static void initialize(String fxmlPath, Stage mainStage) throws IOException {
-        initializeAbsolute(defaultPath + fxmlPath, mainStage);
+    public static void initialize(String fxmlPath, Stage mainStage) throws IOException { initialize(fxmlPath, mainStage, null); }
+    public static <T> void initialize(String fxmlPath, Stage mainStage, Consumer<T> controllerSetup) throws IOException {
+        initializeAbsolute(defaultPath + fxmlPath, mainStage, controllerSetup);
     }
 
-    private static void goToAbsoluteNoHistory(String fxmlPath) {
+    private static void switchView(ViewContext context) {
         try {
-            Parent newRoot = loadFXML(fxmlPath);
-            newRoot.setUserData(fxmlPath);
+            Parent newRoot = loadFXML(context);
             mainScene.setRoot(newRoot);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to load view: " + fxmlPath, e);
+            throw new RuntimeException("Failed to load view: " + context.fxmlPath, e);
         }
     }
 
-    public static void goToAbsoluteIrreversible(String fxmlPath) {
+    public static void goToAbsoluteIrreversible(String fxmlPath) { goToAbsoluteIrreversible(fxmlPath, null); }
+    public static <T> void goToAbsoluteIrreversible(String fxmlPath, Consumer<T> controllerSetup) {
         forwardHistory.clear();
-        goToAbsoluteNoHistory(fxmlPath);
+        switchView(new ViewContext(fxmlPath, controllerSetup));
     }
 
-    public static void goToAbsolute(String fxmlPath) {
-        backHistory.push((String)mainScene.getRoot().getUserData());
-        goToAbsoluteIrreversible(fxmlPath);
+    public static void goToAbsolute(String fxmlPath) { goToAbsolute(fxmlPath, null); }
+    public static <T> void goToAbsolute(String fxmlPath, Consumer<T> controllerSetup) {
+        backHistory.push((ViewContext) mainScene.getRoot().getUserData());
+        goToAbsoluteIrreversible(fxmlPath, controllerSetup);
     }
 
-    public static void goTo(String fxmlPath) {
-        goToAbsolute(defaultPath + fxmlPath);
+    public static void goTo(String fxmlPath) { goTo(fxmlPath, null); }
+    public static <T> void goTo(String fxmlPath, Consumer<T> controllerSetup) {
+        goToAbsolute(defaultPath + fxmlPath, controllerSetup);
     }
 
-    public static void goToIrreversible(String fxmlPath) {
-        goToAbsoluteIrreversible(defaultPath + fxmlPath);
+    public static void goToIrreversible(String fxmlPath) { goTo(fxmlPath, null); }
+    public static <T> void goToIrreversible(String fxmlPath, Consumer<T> controllerSetup) {
+        goToAbsoluteIrreversible(defaultPath + fxmlPath, controllerSetup);
     }
 
     public static void goBack() {
         if (!backHistory.empty()) {
-            forwardHistory.push((String)mainScene.getRoot().getUserData());
-            goToAbsoluteNoHistory(backHistory.pop());
+            forwardHistory.push((ViewContext) mainScene.getRoot().getUserData());
+            switchView(backHistory.pop());
         }
     }
 
     public static void goForward() {
         if (!forwardHistory.empty()) {
-            backHistory.push((String)mainScene.getRoot().getUserData());
-            goToAbsoluteNoHistory(forwardHistory.pop());
+            backHistory.push((ViewContext) mainScene.getRoot().getUserData());
+            switchView(forwardHistory.pop());
         }
     }
 }

35028b989e9dfddb8533e987f6c0c42c43b3c028
Joel Heath <joelheath@outlook.com>
2026-02-13 13:35:50 +0000

    PreferencesModel Tests updated

--- src/test/java/uk/ac/soton/comp2300/group42/energyclient/ui/model/PreferencesModelTest.java
+++ src/test/java/uk/ac/soton/comp2300/group42/energyclient/ui/model/PreferencesModelTest.java
@@ -2,18 +2,24 @@ package uk.ac.soton.comp2300.group42.energyclient.ui.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.ac.soton.comp2300.group42.energyclient.data.api.ColorVision;
 import uk.ac.soton.comp2300.group42.energyclient.data.api.Mode;
 import uk.ac.soton.comp2300.group42.energyclient.data.api.Theme;
 import uk.ac.soton.comp2300.group42.energyclient.data.dto.PreferencesDTO;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class PreferencesModelTest {
     PreferencesDTO dto;
     PreferencesModel model;
-    @Mock HouseModel mockHouse;
+
+    @Mock HouseModel mockHouse1;
+    @Mock HouseModel mockHouse2;
 
     @BeforeEach void setUp() {
         dto = new PreferencesDTO(
@@ -23,27 +29,34 @@ public class PreferencesModelTest {
                 Mode.ADVANCED,
                 true,
                 5.2,
-                -1L);
+                1L);
 
-        model = new PreferencesModel(dto, mockHouse);
+        model = new PreferencesModel(dto, mockHouse1);
     }
 
     @Test void testGetters() {
-        assertEquals(model.getLargeFont(), dto.getLargeFont(), "Large Font should match DTO");
-        assertEquals(model.getVision(), dto.getVision(), "Color Vision should match DTO");
-        assertEquals(model.getTheme(), dto.getTheme(), "Theme should match DTO");
-        assertEquals(model.getMode(), dto.getMode(), "Mode should match DTO");
-        assertEquals(model.getShareLocation(), dto.getShareLocation(), "Share Location should match DTO");
-        assertEquals(model.getEnergyGoal(), dto.getEnergyGoal(), 0.001, "Energy Goal should match DTO");
+        when(mockHouse1.getId()).thenReturn(1L);
+
+        assertEquals(dto.getLargeFont(), model.getLargeFont(), "Large Font should match DTO");
+        assertEquals(dto.getVision(), model.getVision(), "Color Vision should match DTO");
+        assertEquals(dto.getTheme(), model.getTheme(), "Theme should match DTO");
+        assertEquals(dto.getMode(), model.getMode(), "Mode should match DTO");
+        assertEquals(dto.getShareLocation(), model.getShareLocation(), "Share Location should match DTO");
+        assertEquals(dto.getEnergyGoal(), model.getEnergyGoal(), 0.001, "Energy Goal should match DTO");
+        assertEquals(dto.getActiveHouseId(), model.getActiveHouse().getId(), "Active House ID should match DTO");
+        assertEquals(mockHouse1, model.getActiveHouse(), "Active House should match House given in constructor");
     }
 
     @Test void testSetters() {
+        when(mockHouse2.getId()).thenReturn(2L);
+
         model.setLargeFont(false);
         model.setVision(ColorVision.ACHROMA);
         model.setTheme(Theme.DARK);
         model.setMode(Mode.SIMPLE);
         model.setShareLocation(false);
         model.setEnergyGoal(8.7);
+        model.setActiveHouse(mockHouse2);
 
         assertFalse(model.getLargeFont());
         assertEquals(ColorVision.ACHROMA, model.getVision());
@@ -51,6 +64,8 @@ public class PreferencesModelTest {
         assertEquals(Mode.SIMPLE, model.getMode());
         assertFalse(model.getShareLocation());
         assertEquals(8.7, model.getEnergyGoal());
+        assertEquals(mockHouse2, model.getActiveHouse());
+        assertEquals(mockHouse2.getId(), model.getActiveHouse().getId());
     }
 
     @Test void testProperties() {
@@ -60,15 +75,19 @@ public class PreferencesModelTest {
         assertNotNull(model.modeProperty());
         assertNotNull(model.shareLocationProperty());
         assertNotNull(model.energyGoalProperty());
+        assertNotNull(model.activeHouseProperty());
     }
 
     @Test void testCommit() {
+        when(mockHouse2.getId()).thenReturn(2L);
+
         model.setLargeFont(true);
         model.setVision(ColorVision.PROTAN);
         model.setTheme(Theme.DARK);
         model.setMode(Mode.ADVANCED);
         model.setShareLocation(true);
         model.setEnergyGoal(4.5);
+        model.setActiveHouse(mockHouse2);
 
         PreferencesDTO result = model.commit();
 
@@ -78,9 +97,12 @@ public class PreferencesModelTest {
         assertEquals(Mode.ADVANCED, result.getMode(), "DTO Mode should be updated");
         assertTrue(result.getShareLocation(), "DTO Share Location should be updated");
         assertEquals(4.5, result.getEnergyGoal(), 0.001, "DTO Energy Goal should be updated");
+        assertEquals(mockHouse2.getId(), result.getActiveHouseId(), "DTO Active House ID should be updated");
     }
 
     @Test void testUpdateFrom() {
+        when(mockHouse2.getId()).thenReturn(2L);
+
         PreferencesDTO newDto = new PreferencesDTO(
             true,
             ColorVision.DEUTERAN,
@@ -88,9 +110,9 @@ public class PreferencesModelTest {
             Mode.ADVANCED,
             true,
             9.1,
-                -1L);
+                2L);
 
-        model.updateFrom(newDto, mockHouse);
+        model.updateFrom(newDto, mockHouse2);
 
         assertTrue(model.getLargeFont(), "Model LargeFont should be updated from new DTO");
         assertEquals(ColorVision.DEUTERAN, model.getVision(), "Model Vision should be updated from new DTO");
@@ -98,5 +120,7 @@ public class PreferencesModelTest {
         assertEquals(Mode.ADVANCED, model.getMode(), "Model Mode should be updated from new DTO");
         assertTrue(model.getShareLocation(), "Model ShareLocation should be updated from new DTO");
         assertEquals(9.1, model.getEnergyGoal(), 0.001, "Model EnergyGoal should be updated from new DTO");
+        assertEquals(mockHouse2, model.getActiveHouse(), "Model Active House should be updated to from House model");
+        assertEquals(mockHouse2.getId(), model.getActiveHouse().getId(), "Model Active House ID should be updated from new DTO");
     }
 }

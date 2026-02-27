210bcf59706d9ecf1e23a22b801e39c178a7ff46
alexp <alexp@AP2>
2026-02-27 18:29:45 +0000

    Changed fields of MetricsResponse to simplify setup.

--- src/main/java/uk/ac/soton/comp2300/group42/metrics/MetricsResponse.java
+++ src/main/java/uk/ac/soton/comp2300/group42/metrics/MetricsResponse.java
@@ -3,7 +3,7 @@ package uk.ac.soton.comp2300.group42.metrics;
 import java.util.List;
 
 public record MetricsResponse(
-       Long houseId,
-       Long date,
-       List<Double> metrics
+        Long id,
+        Long houseId,
+        List<Double> metrics
 ) {}

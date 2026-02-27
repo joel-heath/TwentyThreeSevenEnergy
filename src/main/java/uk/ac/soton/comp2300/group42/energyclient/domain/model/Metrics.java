465027b5d6b820a825c3befac6f7c9966e5305a1
alexp <alexp@AP2>
2026-02-27 18:25:56 +0000

    Changed fields of metrics alongside API contracts

--- src/main/java/uk/ac/soton/comp2300/group42/energyclient/domain/model/Metrics.java
+++ src/main/java/uk/ac/soton/comp2300/group42/energyclient/domain/model/Metrics.java
@@ -3,7 +3,7 @@ package uk.ac.soton.comp2300.group42.energyclient.domain.model;
 import java.util.List;
 
 public record Metrics(
+    Long id,
     Long houseID,
-    Long date,
     List<Double> metrics
 ) {}

7682a31b909c918c354affd2c1e883d280609544
Isaac <theisaacg123@gmail.com>
2026-03-05 14:34:28 +0000

    converter tests completed and working

--- src/main/java/uk/ac/soton/comp2300/group42/energyserver/repository/UserRepository.java
+++ src/main/java/uk/ac/soton/comp2300/group42/energyserver/repository/UserRepository.java
@@ -2,7 +2,6 @@ package uk.ac.soton.comp2300.group42.energyserver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.ac.soton.comp2300.group42.energyserver.model.User;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {

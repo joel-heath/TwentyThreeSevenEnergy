3a6799e34e5241a5613daca318f0829966fc8105
josh <jg6g24@soton.ac.uk>
2026-04-22 15:33:07 +0100

    Reworked Cost Goal update alert to be better displayed

--- src/main/java/uk/ac/soton/comp2300/group42/energyclient/presentation/viewmodel/SettingsViewModel.java
+++ src/main/java/uk/ac/soton/comp2300/group42/energyclient/presentation/viewmodel/SettingsViewModel.java
@@ -6,15 +6,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import uk.ac.soton.comp2300.group42.energyclient.domain.session.SessionManager;
 import uk.ac.soton.comp2300.group42.energyclient.presentation.observable.ObservablePreferences;
-import uk.ac.soton.comp2300.group42.energyclient.presentation.util.InputFeedbackManager;
 import uk.ac.soton.comp2300.group42.preferences.Mode;
 import uk.ac.soton.comp2300.group42.preferences.Theme;
 
+import java.util.function.BiConsumer;
+
 public class SettingsViewModel {
+
+    private BiConsumer<String, String> alertCallback;
 
     private final SessionManager sessionManager;
     private final ObservablePreferences preferences;
-    private final InputFeedbackManager inputFeedbackManager;
 
     private final ObservableList<Theme> availableThemes;
     private final ObservableList<Mode> availableModes;
@@ -22,10 +24,11 @@ public class SettingsViewModel {
     private final StringProperty costGoalInput = new SimpleStringProperty("");
     private final BooleanProperty hasCostGoalError = new SimpleBooleanProperty(false);
 
-    @Inject public SettingsViewModel(ObservablePreferences preferences, SessionManager sessionManager, InputFeedbackManager feedbackManager) {
+    public void setAlertCallback(BiConsumer<String, String> alertCallback) { this.alertCallback = alertCallback; }
+
+    @Inject public SettingsViewModel(ObservablePreferences preferences, SessionManager sessionManager) {
         this.sessionManager = sessionManager;
         this.preferences = preferences;
-        this.inputFeedbackManager = feedbackManager;
 
         this.availableThemes = FXCollections.observableArrayList(Theme.values());
         this.availableModes = FXCollections.observableArrayList(Mode.values());
@@ -36,7 +39,9 @@ public class SettingsViewModel {
 
         if (raw.isEmpty()) {
             hasCostGoalError.set(true);
-            inputFeedbackManager.showPopup("Cost goal not updated", "Please enter a cost goal before clicking Set Goal.");
+            if (alertCallback != null) {
+                alertCallback.accept("Cost goal not updated", "Please enter a valid number greater than 0 before clicking 'Set Goal'.");
+            }
             return;
         }
 
@@ -49,11 +54,15 @@ public class SettingsViewModel {
             hasCostGoalError.set(false);
             costGoalInput.set("");
 
-            inputFeedbackManager.showPopup("Cost goal updated", String.format("Your new cost goal is £%.2f.", value));
+            if (alertCallback != null) {
+                alertCallback.accept("Cost goal updated", String.format("Your new cost goal is £%.2f.", value));
+            }
 
         } catch (NumberFormatException e) {
             hasCostGoalError.set(true);
-            inputFeedbackManager.showPopup("Cost goal not updated", "Please enter a valid number greater than 0.");
+            if (alertCallback != null) {
+                alertCallback.accept("Cost goal not updated", "Please enter a valid number greater than 0 before clicking 'Set Goal'.");
+            }
         }
     }
 

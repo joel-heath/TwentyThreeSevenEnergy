77dfae067b85195e959355c26f9f4eaf7be41b12
Isaac <theisaacg123@gmail.com>
2026-04-23 13:17:25 +0100

    text of energy status bar changed to black on dark modes to show up better

--- src/main/java/uk/ac/soton/comp2300/group42/energyclient/presentation/controller/AdvancedDashboardController.java
+++ src/main/java/uk/ac/soton/comp2300/group42/energyclient/presentation/controller/AdvancedDashboardController.java
@@ -7,32 +7,30 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import uk.ac.soton.comp2300.group42.energyclient.presentation.util.Navigator;
 import uk.ac.soton.comp2300.group42.energyclient.presentation.view.components.ActivationEditModal;
-import uk.ac.soton.comp2300.group42.energyclient.presentation.view.components.CurrentWeatherWidget;
 import uk.ac.soton.comp2300.group42.energyclient.presentation.view.components.EnergyUsageWidget;
 import uk.ac.soton.comp2300.group42.energyclient.presentation.view.components.UpcomingActivationsWidget;
-import uk.ac.soton.comp2300.group42.energyclient.presentation.viewmodel.*;
+import uk.ac.soton.comp2300.group42.energyclient.presentation.viewmodel.ActivationEditViewModel;
+import uk.ac.soton.comp2300.group42.energyclient.presentation.viewmodel.AdvancedDashboardViewModel;
+import uk.ac.soton.comp2300.group42.energyclient.presentation.viewmodel.EnergyUsageViewModel;
+import uk.ac.soton.comp2300.group42.energyclient.presentation.viewmodel.UpcomingActivationsViewModel;
 
 public class AdvancedDashboardController {
 
     @FXML private EnergyUsageWidget energyWidget;
-    @FXML private CurrentWeatherWidget currentWeatherWidget;
     @FXML private UpcomingActivationsWidget upcomingActivationsWidget;
     @FXML private ActivationEditModal activationEditModal;
     @FXML private HBox statusRow;
 
     private final AdvancedDashboardViewModel vm;
-    private final CurrentWeatherViewModel weatherVM;
     private final EnergyUsageViewModel energyWidgetVM;
     private final UpcomingActivationsViewModel activationsWidgetVM;
     private final ActivationEditViewModel editModalVM;
 
     @Inject public AdvancedDashboardController(AdvancedDashboardViewModel vm,
-                                               CurrentWeatherViewModel weatherVM,
                                                EnergyUsageViewModel energyWidgetVM,
                                                UpcomingActivationsViewModel activationsWidgetVM,
                                                ActivationEditViewModel editModalVM) {
         this.vm = vm;
-        this.weatherVM = weatherVM;
         this.energyWidgetVM = energyWidgetVM;
         this.activationsWidgetVM = activationsWidgetVM;
         this.editModalVM = editModalVM;
@@ -40,7 +38,6 @@ public class AdvancedDashboardController {
 
     @FXML private void initialize() {
         energyWidget.bindComponents(energyWidgetVM);
-        currentWeatherWidget.bindComponents(weatherVM);
 
         upcomingActivationsWidget.bindComponents(activationsWidgetVM, editModalVM, activationEditModal);
 
@@ -69,9 +66,10 @@ public class AdvancedDashboardController {
         card.getStyleClass().addAll("status-card", state.statusStyleClass());
 
         Label statusLabel = new Label(state.emoji());
+        statusLabel.setStyle("-fx-text-fill: -fx-app-status-row-text;");
 
         Label timeLabel = new Label(state.timeText());
-        timeLabel.setStyle("-fx-font-size: 9px;");
+        timeLabel.setStyle("-fx-font-size: 9px; -fx-text-fill: -fx-app-status-row-text;");
 
         card.getChildren().addAll(statusLabel, timeLabel);
         return card;

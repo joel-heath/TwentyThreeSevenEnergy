3892dedf9cd5b15f23d89833aa5401dd7e98d36d
Joel Heath <joelheath@outlook.com>
2026-04-28 22:53:36 +0100

    Removed Weather Widget from Simple Dashboard

--- src/main/java/uk/ac/soton/comp2300/group42/energyclient/presentation/controller/SimpleDashboardController.java
+++ src/main/java/uk/ac/soton/comp2300/group42/energyclient/presentation/controller/SimpleDashboardController.java
@@ -6,35 +6,29 @@ import javafx.fxml.FXML;
 import uk.ac.soton.comp2300.group42.energyclient.presentation.util.Navigator;
 import uk.ac.soton.comp2300.group42.energyclient.presentation.view.components.*;
 import uk.ac.soton.comp2300.group42.energyclient.presentation.viewmodel.ActivationEditViewModel;
-import uk.ac.soton.comp2300.group42.energyclient.presentation.viewmodel.CurrentWeatherViewModel;
 import uk.ac.soton.comp2300.group42.energyclient.presentation.viewmodel.EnergyUsageViewModel;
 import uk.ac.soton.comp2300.group42.energyclient.presentation.viewmodel.UpcomingActivationsViewModel;
 
 public class SimpleDashboardController {
 
     @FXML private EnergyUsageWidget energyWidget;
-    @FXML private CurrentWeatherWidget currentWeatherWidget;
     @FXML private UpcomingActivationsWidget activationsWidget;
     @FXML private ActivationEditModal activationEditModal;
 
     private final EnergyUsageViewModel energyWidgetVM;
-    private final CurrentWeatherViewModel weatherVM;
     private final UpcomingActivationsViewModel activationsWidgetVM;
     private final ActivationEditViewModel editModalVM;
 
     @Inject public SimpleDashboardController(EnergyUsageViewModel energyWidgetVM,
-                                             CurrentWeatherViewModel weatherVM,
                                              UpcomingActivationsViewModel activationsWidgetVM,
                                              ActivationEditViewModel editModalVM) {
         this.energyWidgetVM = energyWidgetVM;
-        this.weatherVM = weatherVM;
         this.activationsWidgetVM = activationsWidgetVM;
         this.editModalVM = editModalVM;
     }
 
     @FXML private void initialize() {
         energyWidget.bindComponents(energyWidgetVM);
-        currentWeatherWidget.bindComponents(weatherVM);
         activationsWidget.bindComponents(activationsWidgetVM, editModalVM, activationEditModal);
         activationsWidget.loadActivationsAsync();
     }
@@ -42,8 +36,6 @@ public class SimpleDashboardController {
     @FXML private void onManageHouses() {
         Navigator.goTo("ManageHouses.fxml");
     }
-
-
 
     @FXML private void onProgressTracking() {
         Navigator.goTo("ProgressTracking.fxml");
